00c645e886d1441b4214e7921fe2a9c4c0dd6c04
Vu Nguyen Huong <lmht1717@gmail.com>
2022-09-05 13:24:13 +0700

    Updated

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-$(document).keydown(function(event){
-    if(event.keyCode==123){
-        return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
-        return false;
-    }
-});
+// $(document).keydown(function(event){
+//     if(event.keyCode==123){
+//         return false;
+//     } else if (event.ctrlKey && event.shiftKey && event.keyCode==73){        
+//         return false;
+//     }
+// });
 
 $(document).on("contextmenu",function(e){        
    e.preventDefault();
 });
+
+document.onkeydown = function(e) {
+    if (e.ctrlKey && 
+        (e.keyCode === 67 || 
+         e.keyCode === 86 || 
+         e.keyCode === 85 || 
+         e.keyCode === 117)) {
+        return false;
+    } else {
+        return true;
+    }
+};
