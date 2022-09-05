403cf022d993ebe50645bfe620f4781e9c447272
Vu Nguyen Huong <lmht1717@gmail.com>
2022-09-06 00:02:32 +0700

    Updated

--- js/main.js
+++ js/main.js
@@ -21,3 +21,26 @@ document.onkeydown = function(e) {
         return true;
     }
 };
+
+
+
+
+
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "108688041898641");
+chatbox.setAttribute("attribution", "biz_inbox");
+
+window.fbAsyncInit = function() {
+    FB.init({
+        xfbml            : true,
+        version          : 'v14.0'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
